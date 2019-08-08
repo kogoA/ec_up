f4b2b4c0f56400933f1f753fdf284720cd0a082f
rio <kaihatukogo@gmail.com>
2019-08-08 16:58:21 +0900

    update css ver2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,17 @@ class ApplicationController < ActionController::Base
     if resource.is_a?(User)
       root_path
     elsif resource.is_a?(Admin)
-      admins_root
+      admins_root_path
     end
   end
 
-  layout 'login_admin'
-  def admins_root
-    admins_root_path
-  end
-
+  private
   # deviseの管理者権限logout後のリダイレクト先指定
   def after_sign_out_path_for(resource)
-    admins_root_path # ログアウト後に遷移するpathを設定
+    if resource == :user
+      root_path
+    else resource == :admin
+      new_admin_session_path
+    end
   end
 end
