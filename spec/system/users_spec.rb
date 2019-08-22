ffe20cf95405202a5a8f686fc2cb42eccdcd9e1c
rio <kaihatukogo@gmail.com>
2019-08-22 22:54:53 +0900

    update systemspec test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,35 +6,38 @@ RSpec.describe 'Users', type: :system do
     user.confirmed_at = Time.now
     user.save
   end
-     context 'can login' do
-     scenario 'ログインする' do
-    # トップページを開く
-    visit root_path
-    click_on 'ログイン'
-    fill_in 'メールアドレス', with: 'foobar@amail.com'
-    fill_in 'パスワード', with: 'password'
-    click_on 'ログイン'
-    first(:link, '詳細').click
-    click_on('買い物かごに入れる')
-    click_on('park gallery')
-    all(:link, '詳細')[3].click
-    click_on('買い物かごに入れる')
-    click_on('park gallery')
-    click_on('shopping_cart')
-    click_on('商品のお支払い')
-    sleep 1
-    page.within_frame(all('iframe')[0]) do
-  fill_in 'メールアドレス', with: 'fo@ba.om'
-  fill_in 'カード番号', with: '4242424242424242'
-  fill_in '有効期限', with: '0222'
-  fill_in 'CVC', with: '222'
-  click_on 'を支払う'
-  visit root_path
-  click_on 'ログアウト'
-  # binding.pry
-  # expect(page).to have_content '買い物かごに入れる'
-  # save_screenshot
-end
-     end
-  end
+    context 'can login' do
+      scenario 'ログインする' do
+        # トップページを開く
+        visit root_path#
+        click_on 'ログイン'
+        fill_in 'メールアドレス', with: 'foobar@amail.com'
+        fill_in 'パスワード', with: 'password'
+        click_on 'ログイン'
+        #ログイン成功
+        first(:link, '詳細').click
+        expect(page).to have_content '買い物かごに入れる'
+        click_on('買い物かごに入れる')
+        click_on('park gallery')
+        all(:link, '詳細')[3].click
+        click_on('買い物かごに入れる')
+        click_on('park gallery')
+        click_on('shopping_cart')
+        expect(page).to have_content '商品のお支払い'
+        click_on('商品のお支払い')
+        sleep 1
+          page.within_frame(all('iframe')[0]) do
+            fill_in 'メールアドレス', with: 'fo@ba.om'
+            fill_in 'カード番号', with: '4242424242424242'
+            fill_in '有効期限', with: '0222'
+            fill_in 'CVC', with: '222'
+            click_on 'を支払う'
+          end
+        visit root_path
+        click_on 'ログアウト'
+        expect(page).to have_content 'ログイン'
+        # binding.pry
+        # save_screenshot
+      end
+    end
 end
