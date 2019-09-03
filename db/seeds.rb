9114e67c2e94d5612d88d03df84d2ae2adec8b92
Kogo Araki <elle_men@yahoo.co.jp>
2019-09-03 23:09:00 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(email: 'aaaaaaaa@gmail.com', password: 'password')
+
 
 20.times do |n|
   Product.create!(
